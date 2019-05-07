4186ec452c772801c4e4361ef38ee1c531cad4f3
Simon Jarbrant <simon.jarbrant@isotop.se>
2019-05-07 16:27:13 +0200

    Initial commit

---
+++ Sources/Endpoints.h
@@ -0,0 +1,18 @@
+//
+//  Endpoints.h
+//  Endpoints
+//
+//  Created by Simon Jarbrant on 2019-05-07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Endpoints.
+FOUNDATION_EXPORT double EndpointsVersionNumber;
+
+//! Project version string for Endpoints.
+FOUNDATION_EXPORT const unsigned char EndpointsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Endpoints/PublicHeader.h>
+
+
