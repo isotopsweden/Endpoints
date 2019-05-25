263026633f6df819c3f6917b83ea027e0aee6c7a
Simon Jarbrant <simonjarbrant@me.com>
2019-05-25 17:16:41 +0200

    Introduce the Transporter protocol

--- Sources/ResponseParser.swift
+++ Sources/ResponseParser.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+extension HTTPURLResponse {
+    static var successfulStatusCode: Range<Int> {
+        return 200..<300
+    }
+}
+
 public struct ResponseParser<Unpacker: DataUnpacker> {
     private let unpacker: Unpacker
 
@@ -14,12 +20,20 @@ public struct ResponseParser<Unpacker: DataUnpacker> {
         self.unpacker = unpacker
     }
 
-    public func parseResponse(response: HTTPURLResponse, data: Data) throws -> Unpacker.DataType {
+    public func parseResponse(response: HTTPURLResponse, data: Data) throws -> CommunicatorResponse<Unpacker.DataType> {
         switch response.statusCode {
         case HTTPURLResponse.successfulStatusCode:
-            return try unpacker.unpack(data)
+            let decodedData = try unpacker.unpack(data)
+            let response = CommunicatorResponse(
+                headers: response.allHeaderFields,
+                code: response.statusCode,
+                body: decodedData)
+
+            return response
+
         case 400..<500:
             throw CommunicatorError.unacceptableStatusCode(.clientError(code: response.statusCode, data: data))
+
         default:
             throw CommunicatorError.unacceptableStatusCode(.serverError(code: response.statusCode))
         }
