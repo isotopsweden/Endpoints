75192ee36e34527c187c18e384221dcf040ee86d
Simon Jarbrant <simon.jarbrant@isotop.se>
2019-09-03 12:56:48 +0200

    Add platforms declaration to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Endpoints",
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v10_14)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
