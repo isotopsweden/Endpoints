4c0c8a2a2ae96a37c88e81db42902196990511ef
Simon Jarbrant <simon.jarbrant@isotop.se>
2019-10-29 15:46:52 +0100

    Add unit test targets for iOS and macOS

---
+++ Tests/EndpointsTests.swift
@@ -0,0 +1,26 @@
+//
+//  EndpointsTests.swift
+//  EndpointsTests
+//
+//  Created by Simon Jarbrant on 2019-10-29.
+//
+
+import XCTest
+
+@testable import Endpoints
+
+class EndpointsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
